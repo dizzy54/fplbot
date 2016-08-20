fcc1cf5dd0abdd56d74d0c60b9d41efaf7ef490e
Akshat <akshat54@gmail.com>
2016-08-20 19:14:03 +0530

    player model created

--- analysis/models.py
+++ analysis/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    """Class to hold all player attributes
+    """
+    fpl_id = models.IntegerField('player id in fpl db')
+    first_name = models.IntegerField('player first name')
+    last_name = models.IntegerField('payer last name')
