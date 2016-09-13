b0ebfe0d08ce4c90806b800f79856a6bd2e07757
Akshat <akshat54@gmail.com>
2016-09-13 19:43:53 +0530

    project dir corrected

--- fplbot/settings.py
+++ fplbot/settings.py
@@ -141,6 +141,6 @@ DATABASES['default'].update(db_from_env)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
