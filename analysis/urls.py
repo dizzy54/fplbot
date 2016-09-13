fff744c2eb93ad7d7d93ed0a13fb2f23aefb8dc5
Akshat <akshat54@gmail.com>
2016-09-13 19:08:39 +0530

    debug

--- analysis/urls.py
+++ analysis/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import PredictView
 
 urlpatterns = [
-    url(r'^predict_fb/', PredictView.asView()),
+    url(r'^predict_fb/', PredictView.as_view()),
 ]
--- fplbot/settings.py
+++ fplbot/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import sys
 import dj_database_url
+from ignored import keys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +25,7 @@ sys.path.append(os.path.join(BASE_DIR, 'lib'))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default='')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=keys.keys['DJANGO_SECRET_KEY'])
 
 # custom keys
 PAGE_ACCESS_TOKEN = os.getenv('FB_PAGE_TOKEN', default='')
